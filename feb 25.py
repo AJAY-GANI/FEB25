8da7a023088d8cbf138c0f6bbe2b45ebc6a35e79
AJAY-GANI <99253254+AJAY-GANI@users.noreply.github.com>
2022-02-25 22:26:44 +0530

    Add files via upload

---
+++ feb 25.py
@@ -0,0 +1,7 @@
+n=int(input("enter a value"))
+l=[ ]
+for i in range(n):
+    x=int(input())
+    l.append(x)
+l.sort
+print(l)
---
+++ feb25.py
@@ -0,0 +1,12 @@
+n=int(input())
+l=[ ]
+x=[ ]
+for i in range(n):
+    t=int(input())
+    l.append
+    l.append(t)
+for i in l:
+    if i !=0:
+        x.append(i)
+print(x)
+print(len(x))
